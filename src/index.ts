2eb5db9b3d5733413e91227074685f1a19ee41b5
ValentinGTrapaga <valentingt22@gmail.com>
2024-06-19 03:53:25 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
+
 import { authRouter } from './routers/auth'
 import { usersRouter } from './routers/admin/users'
-import { verifyToken } from './middlewares/verifyToken'
-import { isAdmin } from './middlewares/isAdmin'
 import { transcriptionsRouter } from './routers/trancriptions'
 import { translationsRouter } from './routers/translations'
 import { friendlifyRouter } from './routers/friendlify'
 
+import { isAdmin } from './middlewares/isAdmin'
+import { verifyToken } from './middlewares/verifyToken'
+
 const app = express()
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 app.use(express.json())
