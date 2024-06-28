3a21997ecd53a6153d50650fd2aef2af33ededf1
RamunnoAJ <ramunnoagustin@gmail.com>
2024-06-28 14:22:16 -0300

    feat: implement creation of images

--- src/libs/images/generate.ts
+++ src/libs/images/generate.ts
@@ -1,39 +1,66 @@
-import { createCanvas, loadImage } from 'canvas'
+import { createCanvas, loadImage } from "canvas";
 
 export async function mergeTextWithImage(
   imagePath: string,
-  text: string
+  text: string,
+  options: any,
 ): Promise<Buffer | undefined> {
   try {
-    const image = await loadImage(imagePath)
-    const width = image.width
-    const height = image.height
+    const image = await loadImage(imagePath);
+    const { width, height } = image;
 
-    const canvas = createCanvas(width, height)
-    const ctx = canvas.getContext('2d')
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext("2d");
 
-    ctx.drawImage(image, 0, 0, width, height)
+    ctx.drawImage(image, 0, 0, width, height);
 
-    const fontSize = 50
-    const fontFamily = 'Montserrat'
-    ctx.font = `${fontSize}px ${fontFamily}`
-    ctx.fillStyle = '#333'
-    ctx.textAlign = 'center'
-    ctx.textBaseline = 'middle'
+    const {
+      fontSize = 50,
+      fontFamily = "Montserrat",
+      textAlign = "center",
+      fillStyle = "#333",
+      textBaseline = "middle",
+    } = options;
 
-    const lines = text.split('\n')
-    const lineHeight = fontSize * 1.2
-    const textHeight = lines.length * lineHeight
+    ctx.font = `${fontSize}px ${fontFamily}`;
+    ctx.fillStyle = fillStyle;
+    ctx.textAlign = textAlign;
+    ctx.textBaseline = textBaseline;
 
-    let y = (height - textHeight) / 2 + lineHeight / 2
+    const lines = text.split("\n");
+    const lineHeight = fontSize * 1.2;
+    const textHeight = lines.length * lineHeight;
+
+    let y = (height - textHeight) / 2 + lineHeight / 2;
+
+    let maxLineWidth = 0;
+    lines.forEach((line) => {
+      const lineWidth = ctx.measureText(line).width;
+      if (lineWidth > maxLineWidth) {
+        maxLineWidth = lineWidth;
+      }
+    });
 
     lines.forEach((line) => {
-      ctx.fillText(line, width / 2, y)
-      y += lineHeight
-    })
+      let x;
+      switch (textAlign) {
+        case "start":
+          x = width / 2 - maxLineWidth / 2;
+          break;
+        case "end":
+          x = width / 2 + maxLineWidth / 2;
+          break;
+        case "center":
+        default:
+          x = width / 2;
+      }
 
-    return canvas.toBuffer('image/png')
+      ctx.fillText(line, x, y);
+      y += lineHeight;
+    });
+
+    return canvas.toBuffer("image/png");
   } catch (error) {
-    console.error('Error merging text with image:', error)
+    console.error("Error merging text with image:", error);
   }
 }
--- src/routers/mediaCreation.ts
+++ src/routers/mediaCreation.ts
@@ -1,9 +1,106 @@
-import { Router } from 'express'
+import fs from "fs";
+import path from "path";
+import multer from "multer";
+import archiver from "archiver";
+import { Router } from "express";
+import { mergeTextWithImage } from "../libs/images/generate";
 
-export const mediaCreationRouter = Router()
+export const mediaCreationRouter = Router();
 
-mediaCreationRouter.post('/upload', (req, res) => {
-  res.json({
-    message: 'Upload successful'
-  })
-})
+const upload = multer({
+  fileFilter: (_, file, cb) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+    }
+  },
+  storage: multer.diskStorage({
+    destination: (_, __, cb) => {
+      fs.mkdir("uploads/", (err) => {
+        cb(null, "uploads/");
+      });
+    },
+    filename: (_, file, cb) => {
+      cb(null, Date.now() + "-" + file.originalname);
+    },
+  }),
+});
+
+mediaCreationRouter.post(
+  "/upload",
+  upload.single("file"),
+  async (req: any, res) => {
+    if (!req.file) {
+      return res.status(400).send("No file uploaded");
+    }
+
+    const texts: string[] = JSON.parse(req.body.texts);
+    const options = JSON.parse(req.body.options);
+    const imagePath: string = req.file.path;
+    const folderPath: string = "uploads/compressed";
+
+    try {
+      if (fs.existsSync(folderPath)) {
+        fs.rmSync(folderPath, { recursive: true });
+      }
+
+      fs.mkdirSync(folderPath, { recursive: true });
+
+      texts.forEach(async (text) => {
+        const imageBuffer = (await mergeTextWithImage(
+          imagePath,
+          text,
+          options,
+        )) as any;
+        const imageFileName = `${new Date().toISOString()}-${text}.png`;
+        const imageFilePath = path.join(folderPath, imageFileName);
+        fs.writeFileSync(imageFilePath, imageBuffer);
+      });
+
+      fs.unlinkSync(imagePath);
+
+      res.send({ path: folderPath });
+    } catch (error) {
+      res.status(500).send("Error generating the image.");
+    }
+  },
+);
+
+//
+
+mediaCreationRouter.get("/download", async (req: any, res) => {
+  try {
+    const folderPath = req.query.folderPath;
+    if (!folderPath) {
+      return res.status(400).send("No path provided.");
+    }
+
+    if (!fs.existsSync(folderPath)) {
+      return res.status(404).send("Path not found.");
+    }
+
+    res.setHeader("Content-Type", "application/zip");
+    res.setHeader("Content-Disposition", "attachment; filename=images.zip");
+
+    const archive = archiver("zip", {
+      zlib: { level: 9 },
+    });
+
+    archive.on("error", (err) => {
+      res.status(500).send({ error: err.message });
+    });
+
+    archive.pipe(res);
+
+    archive.directory(folderPath, false);
+    await archive.finalize();
+  } catch (error) {
+    res.status(500).send("Error downloading the images.");
+  }
+});
