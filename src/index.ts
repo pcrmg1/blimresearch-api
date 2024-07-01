50da1bb71b8294b2eb0e3b15ec971da9c344d069
ValentinGTrapaga <valentingt22@gmail.com>
2024-07-01 18:02:06 -0300

    delete showing token on main

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,43 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import compression from "compression";
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import compression from 'compression'
 
-import { config } from "dotenv";
-config();
+import { config } from 'dotenv'
+config()
 
-import { authRouter } from "./routers/auth";
-import { usersRouter } from "./routers/admin/users";
-import { transcriptionsRouter } from "./routers/trancriptions";
-import { translationsRouter } from "./routers/translations";
-import { friendlifyRouter } from "./routers/friendlify";
-import { mediaCreationRouter } from "./routers/mediaCreation";
-import { isAdmin } from "./middlewares/isAdmin";
-import { verifyToken } from "./middlewares/verifyToken";
-import { viralsRouter } from "./routers/virals";
+import { authRouter } from './routers/auth'
+import { usersRouter } from './routers/admin/users'
+import { transcriptionsRouter } from './routers/trancriptions'
+import { translationsRouter } from './routers/translations'
+import { friendlifyRouter } from './routers/friendlify'
+import { mediaCreationRouter } from './routers/mediaCreation'
+import { isAdmin } from './middlewares/isAdmin'
+import { verifyToken } from './middlewares/verifyToken'
+import { viralsRouter } from './routers/virals'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(compression());
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.static(__dirname));
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(compression())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.static(__dirname))
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) =>
+app.get('/', (req, res) =>
   res.json({
-    message: process.env.TOKEN_SECRET,
-  }),
-);
-app.use("/api/auth", authRouter);
-app.use("/api/admin/users", verifyToken, isAdmin, usersRouter);
-app.use("/api/transcriptions", verifyToken, transcriptionsRouter);
-app.use("/api/translations", verifyToken, translationsRouter);
-app.use("/api/friendlify", verifyToken, friendlifyRouter);
-app.use("/api/virals", verifyToken, viralsRouter);
-app.use("/api/mediaCreation", verifyToken, mediaCreationRouter);
+    message: 'Hello from blimAPI 🚀'
+  })
+)
+app.use('/api/auth', authRouter)
+app.use('/api/admin/users', verifyToken, isAdmin, usersRouter)
+app.use('/api/transcriptions', verifyToken, transcriptionsRouter)
+app.use('/api/translations', verifyToken, translationsRouter)
+app.use('/api/friendlify', verifyToken, friendlifyRouter)
+app.use('/api/virals', verifyToken, viralsRouter)
+app.use('/api/mediaCreation', verifyToken, mediaCreationRouter)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`⚡️: Server running at port: ${PORT}`));
+app.listen(PORT, () => console.log(`⚡️: Server running at port: ${PORT}`))
