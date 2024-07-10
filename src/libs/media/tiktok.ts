ef20810c6627ff6ccdafd4a13850aed113ed0b90
ValentinGTrapaga <valentingt22@gmail.com>
2024-07-10 11:15:47 -0300

    switch to version v2 to download tiktok

--- src/libs/media/tiktok.ts
+++ src/libs/media/tiktok.ts
@@ -13,14 +13,14 @@ export const transcribeTiktokVideo = async ({
 }) => {
   try {
     const id = url?.split('/')[5].split('?')[0] as string
-    const videoLink = await Tiktok.Downloader(url, { version: 'v3' })
+    const videoLink = await Tiktok.Downloader(url, { version: 'v2' })
     console.log({ videoLink })
     const filename = `${id}.mp3`
-    if (!videoLink || !videoLink.result || !videoLink.result.video2)
+    if (!videoLink || !videoLink.result || !videoLink.result.video)
       throw new Error('No se pudo descargar el video')
     console.log('Descargando video ', filename)
     await downloadVideoFromUrl({
-      url: videoLink.result.video2,
+      url: videoLink.result.video,
       filename: `${id}.mp4`
     })
     console.log('Video descargado, extrayendo audio')
