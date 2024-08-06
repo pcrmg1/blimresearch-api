23a8bb154fbd2ed593c478070ab1a095f8efcca5
RamunnoAJ <ramunnoagustin@gmail.com>
2024-08-06 13:46:11 -0300

    fix: query params parse

--- src/routers/admin/users.ts
+++ src/routers/admin/users.ts
@@ -1,69 +1,71 @@
-import { Router } from 'express'
-import { createUser, getAllUsers, getUsersCount } from '../../db/user'
-import { QueryParamsSchema } from '../../models/queryParams'
-import { NewUserSchema } from '../../models/user'
-import { hashPassword } from '../../utils/password'
+import { Router } from "express";
+import { createUser, getAllUsers, getUsersCount } from "../../db/user";
+import { QueryParamsSchema } from "../../models/queryParams";
+import { NewUserSchema } from "../../models/user";
+import { hashPassword } from "../../utils/password";
 
-export const adminUsersRouter = Router()
+export const adminUsersRouter = Router();
 
-adminUsersRouter.post('/createUser', async (req, res) => {
+adminUsersRouter.post("/createUser", async (req, res) => {
   try {
-    const { email, password, role, name } = req.body
+    const { email, password, role, name } = req.body;
     const parsedCredentials = await NewUserSchema.safeParseAsync({
       email,
       password,
       role,
-      name
-    })
+      name,
+    });
     if (!parsedCredentials.success) {
       return res.status(400).json({
-        message: 'Credenciales no validas para crear el usuario'
-      })
+        message: "Credenciales no validas para crear el usuario",
+      });
     }
-    const hashedPassword = await hashPassword({ password })
+    const hashedPassword = await hashPassword({ password });
     const user = createUser({
       email,
       passwordHash: hashedPassword,
       role,
-      name
-    })
-    return res.json(user)
+      name,
+    });
+    return res.json(user);
   } catch (error) {
-    return res.status(500).json({ message: 'Internal server error' })
+    return res.status(500).json({ message: "Internal server error" });
   }
-})
+});
 
-adminUsersRouter.get('/', async (req, res) => {
-  const { page, limit, query, orderBy } = req.query
+adminUsersRouter.get("/", async (req, res) => {
+  const { page, limit, query, orderBy } = req.query;
   try {
     const parsedQuery = await QueryParamsSchema.safeParseAsync({
-      page: page,
-      limit: limit,
+      page: Number(page),
+      limit: Number(limit),
       query,
-      orderBy
-    })
+      orderBy,
+    });
+    console.log(parsedQuery.error);
     if (!parsedQuery.success) {
-      return res.status(400).json({ message: 'Query params are not valid' })
+      return res.status(400).json({ message: "Query params are not valid" });
     }
 
     const {
       page: parsedPage,
       limit: parsedLimit,
       orderBy: parsedOrderBy,
-      query: parsedQueryString
-    } = parsedQuery.data
+      query: parsedQueryString,
+    } = parsedQuery.data;
     const users = await getAllUsers({
       limit: parsedLimit,
       page: parsedPage,
       query: parsedQueryString,
-      orderBy: parsedOrderBy
-    })
-    const count = await getUsersCount()
-    console.log({ count, brought: parsedPage * parsedLimit + users.length })
-    const nextPage = count > parsedPage * parsedLimit + users.length
-    const prevPage = parsedPage > 0
-    return res.json({ data: users, nextPage, prevPage, count })
+      orderBy: parsedOrderBy,
+    });
+    const count = await getUsersCount();
+    console.log({ count, brought: parsedPage * parsedLimit + users.length });
+    const nextPage = count > parsedPage * parsedLimit + users.length;
+    const prevPage = parsedPage > 0;
+    return res.json({ data: users, nextPage, prevPage, count });
   } catch (error) {
-    return res.status(500).json({ message: 'Internal server error' })
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
   }
-})
+});
