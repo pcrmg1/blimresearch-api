240f286879909d8a0fb97bf7b0d8202b4137cc4d
ValentinGTrapaga <valentingt22@gmail.com>
2024-08-28 12:03:04 -0300

    add information to user and fix support email template

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -4,6 +4,7 @@ import { errorHandler } from '../utils/error'
 import { RequestWithToken } from '../types/jwt'
 import { formatCurrencyToAddToDB } from '../utils/currency'
 import { sendMail } from '../libs/nodemailer/transporter'
+import { prisma } from '../db/prisma'
 
 export const usersRouter = Router()
 
@@ -38,20 +39,46 @@ usersRouter.post('/support', async (req: RequestWithToken, res) => {
     if (!user) {
       return res.status(404).json({ message: 'User not found' })
     }
-    const html = `El usuario ${
+    const html = `<p>El usuario ${
       user.email
-    } ha abierto un ticket de soporte con el siguiente mensaje: ${text}\n
-    Creditos usados del usuario: ${user.creditosUsados}\n
+    } ha abierto un ticket de soporte con el siguiente mensaje: ${text}</p>\n
+    <p>Creditos usados del usuario: ${user.creditosUsados}\n
     Creditos restantes del usuario: ${
       user.limiteCreditos - user.creditosUsados
     }\n
-    Limite de creditos del usuario: ${user.limiteCreditos}\n`
+    Limite de creditos del usuario: ${user.limiteCreditos}</p>\n`
     await sendMail({
       emailTo: 'soporte@blimbooster.com',
       subject: `Support ticket - ${user.email}`,
       html
     })
     return res.json({ message: 'Support ticket sent' })
+  } catch (error) {
+    errorHandler(error)
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+})
+
+usersRouter.put('/', async (req: RequestWithToken, res) => {
+  const userId = req.userId
+  const { field, value } = req.body
+  if (!userId) {
+    return res.status(401).json({ message: 'No userId' })
+  }
+  try {
+    const user = await getUserById({ userId })
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+    if (['information'].includes(field)) {
+      await prisma.user.update({
+        where: { id: userId },
+        data: { [field]: value }
+      })
+      return res.json({ message: 'User updated' })
+    } else {
+      return res.status(400).json({ message: 'Invalid field' })
+    }
   } catch (error) {
     errorHandler(error)
     return res.status(500).json({ message: 'Internal server error' })
