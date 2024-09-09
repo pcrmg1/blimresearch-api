73a7f8e56150ae7c533507debbc282b69f13057e
ValentinGTrapaga <valentingt22@gmail.com>
2024-09-09 13:11:01 -0300

    fix: consts values to look for better results

--- src/consts.ts
+++ src/consts.ts
@@ -5,13 +5,12 @@ export const LANGUAGES_FOR_QUERIES = [
   'italiano'
 ]
 
-export const RESULT_LIMITS_INSTAGRAM = 10
-
+export const RESULT_LIMITS_INSTAGRAM = 30
 //- 5 Créditos por idioma en búsquedas de tiktok
 // - 7 Créditos por idioma en búsqueda de instagram
 // - 30 Créditos por búsqueda lista de virales (Cada 5 usuarios)
 export const CREDITS_COST = {
   busqueda_tiktok: 5,
-  busqueda_instagram: 7,
+  busqueda_instagram: 10,
   busqueda_virales_cada_5: 30
 }
