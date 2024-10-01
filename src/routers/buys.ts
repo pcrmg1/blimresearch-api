debaa40aebb830116049d1599594f5daebed2920
ValentinGTrapaga <valentingt22@gmail.com>
2024-10-01 18:04:48 -0300

    add coralmujaes router

---
+++ src/routers/buys.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import { appendToFile } from '../utils/writeTxt'
+
+export const buysRouter = Router()
+
+buysRouter.post('/coralmujaesweb', async (req, res) => {
+  const { body } = req
+  try {
+    console.log('Escribiendo en archivo...')
+    await appendToFile('src/data/coralmujaesweb.txt', body)
+    return res
+      .status(200)
+      .json({ message: 'Data written successfully', data: body })
+  } catch (error) {
+    console.error('Error writing to file:', error)
+    return res.status(500).json({ message: 'Error writing to file' })
+  }
+})
---
+++ src/utils/writeTxt.ts
@@ -0,0 +1,21 @@
+import { promises as fs } from 'fs'
+
+export async function appendToFile(
+  filePath: string,
+  data: string | object
+): Promise<void> {
+  try {
+    // Open the file in append mode, and create it if it doesn't exist
+    await fs.open(filePath, 'a')
+
+    // Convert object to string if necessary
+    const dataToWrite =
+      typeof data === 'object' ? JSON.stringify(data, null, 2) : data
+
+    // Append the data to the file
+    await fs.appendFile(filePath, `${dataToWrite}\n`)
+    console.log('Data appended successfully')
+  } catch (error) {
+    console.error('Error writing to file:', error)
+  }
+}
