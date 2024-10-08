373379bf5b02773f96aff59c2e9a9be54986e17e
ValentinGTrapaga <valentingt22@gmail.com>
2024-10-08 14:44:20 -0300

    fix: correct data response format

--- src/routers/extension.ts
+++ src/routers/extension.ts
@@ -1,25 +1,30 @@
-import { Router } from "express";
-import { generateTopicsQueries } from "../libs/openai/topics";
+import { Router } from 'express'
+import { generateTopicsQueries } from '../libs/openai/topics'
 
-export const extensionRouter = Router();
+export const extensionRouter = Router()
 
-extensionRouter.post("/topics", async (req, res) => {
-    const {beneficios, problemas, ejemplos} = req.body
-    try {
-        const queryTopics = await generateTopicsQueries({ beneficios, problemas, ejemplos });
-        if(!queryTopics) {
-            return res.status(400).json({ message: "No topics found" });
-        }
-        const topicUrls = queryTopics.topics.slice(0,20).map((query) => {
-            const tiktokUrl = new URL("https://www.tiktok.com/");
-            tiktokUrl.searchParams.append("q", query);
-            return tiktokUrl.toString();
-        })
-        return res.json({ data: {
-            topics: queryTopics.topics,
-            urls: topicUrls
-        } });
-    } catch (error) {
-        return res.status(500).json({ message: "Internal server error" });
-    }
+extensionRouter.post('/topics', async (req, res) => {
+  const { beneficios, problemas, ejemplos } = req.body
+  try {
+    const queryTopics = await generateTopicsQueries({
+      beneficios,
+      problemas,
+      ejemplos
     })
+    if (!queryTopics) {
+      return res.status(400).json({ message: 'No topics found' })
+    }
+    const topicUrls = queryTopics.topics.slice(0, 20).map((query) => {
+      const tiktokUrl = new URL('https://www.tiktok.com/search')
+      tiktokUrl.searchParams.append('q', query)
+      return { url: tiktokUrl.toString(), query }
+    })
+    return res.json({
+      data: {
+        topicUrls
+      }
+    })
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+})
