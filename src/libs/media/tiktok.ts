35124d0125428aa9d7755c767367e8dc09de670f
ValentinGTrapaga <valentingt22@gmail.com>
2024-10-10 19:35:14 -0300

    fix: use apify as downloader

--- src/libs/media/tiktok.ts
+++ src/libs/media/tiktok.ts
@@ -7,33 +7,6 @@ import { getTiktokVideoId } from '../../utils/parser'
 import { getTiktokDataFromPost } from '../apify/tiktok'
 
 export const transcribeTiktokVideo = async ({ url }: { url: string }) => {
-  // try {
-  //   const { videoId: id } = getTiktokVideoId({ url })
-  //   const videoLink = await Tiktok.Downloader(url, { version: 'v2' })
-  //   const filename = `${id}.mp3`
-  //   console.log({ videoLink: videoLink.result, id })
-  //   if (!videoLink || !videoLink.result || !videoLink.result.video)
-  //     throw new Error('No se pudo descargar el video')
-  //   await downloadVideoFromUrl({
-  //     url: videoLink.result.video,
-  //     filename: `${id}.mp4`
-  //   })
-  //   await extractAudio({
-  //     inputPath: `${id}.mp4`,
-  //     outputPath: `${id}.mp3`
-  //   })
-  //   const transcription = await transcribeAudio(filename)
-  //   await unlink(`${id}.mp4`)
-  //   await unlink(`${id}.mp3`)
-  //   return { transcription, videoId: id }
-  // } catch (error) {
-  //   console.log({ error })
-  //   if (error instanceof Error) {
-  //     throw new Error(error.message)
-  //   } else {
-  //     throw new Error('Hubo un error procesando el video')
-  //   }
-  // }
   try {
     const { cost, item } = await getTiktokDataFromPost({ url })
     const { id, musicMeta } = item
@@ -53,17 +26,11 @@ export const transcribeTiktokVideo = async ({ url }: { url: string }) => {
 
 export const getTiktokVideoCaption = async ({ url }: { url: string }) => {
   try {
-    const { videoId: id } = getTiktokVideoId({ url })
-    const videoLink = await Tiktok.Downloader(url, { version: 'v1' })
-    if (!videoLink || !videoLink.result || !videoLink.result.video)
-      throw new Error('No se pudo descargar el video')
-    return { caption: videoLink.result.video.cover[1], videoId: id }
+    const { cost, item } = await getTiktokDataFromPost({ url })
+    const { id, videoMeta } = item
+    return { caption: videoMeta.coverUrl, videoId: id }
   } catch (error) {
     console.log({ error })
-    if (error instanceof Error) {
-      throw new Error(error.message)
-    } else {
-      throw new Error('Hubo un error procesando el video')
-    }
+    throw new Error('Hubo un error procesando el video')
   }
 }
